ad3f9ffdd58c1dc00d8630f8b9e799ab74a59dc2
Harikrishnan K M <harikunjath407@gmail.com>
2023-09-13 09:49:36 +0530

    Initial Commit

---
+++ UI_Practice2/UI_Practice2App.swift
@@ -0,0 +1,17 @@
+//
+//  UI_Practice2App.swift
+//  UI_Practice2
+//
+//  Created by Harikrishnan K M on 13/09/23.
+//
+
+import SwiftUI
+
+@main
+struct UI_Practice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
