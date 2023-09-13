9fc79240dc1d961f8da09e8e5e8eaa1c1a9ee663
Harikrishnan K M <harikunjath407@gmail.com>
2023-09-13 17:50:56 +0530

    Finished

--- UI_Practice2/GameDetailView.swift
+++ UI_Practice2/GameDetailView.swift
@@ -14,18 +14,28 @@ struct GameDetailView: View {
     
     var body: some View {
         VStack{
-            cards
+            Text("Let's Play")
+                .font(.headline)
+                .fontWeight(.heavy)
+            ScrollView{
+                cards
+            }
+            Spacer()
             cardCountAdjuster
+            
         }
         .padding()
+        
     }
     
     var cards: some View{
-        HStack {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]){
             ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
+        
         .foregroundColor(.orange)
     }
     
@@ -39,51 +49,47 @@ struct GameDetailView: View {
         .font(.largeTitle)
     }
     
-    var cardRemover: some View{
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
         Button(action: {
-            if cardCount > 1
-            {
-                cardCount -= 1
-            }
+            cardCount += offset
         }, label: {
-            Image(systemName: "plus.circle.fill")
+            Image(systemName: symbol)
         })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        return cardCountAdjuster(by: -1, symbol: "minus.circle.fill")
     }
     
     var cardAdder: some View {
-        Button(action: {
-            if cardCount < emojis.count {
-                cardCount += 1
-            }
-        }, label: {
-            Image(systemName: "minus.circle.fill")
-        })
+        return cardCountAdjuster(by: 1, symbol: "plus.circle.fill")
     }
-}
-
-struct GameDetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        GameDetailView()
-    }
-}
-
-struct CardView: View {
-    var content: String
-    @State var isFaceUp: Bool = true
     
-    var body: some View{
-        ZStack {
-            let base = RoundedRectangle(cornerRadius: 12)
-            if isFaceUp{
-                base.fill(.white)
-                base.strokeBorder(lineWidth: 2)
-                Text(content).font(.largeTitle)
-            } else {
-                base.fill()
-            }
+    struct GameDetailView_Previews: PreviewProvider {
+        static var previews: some View {
+            GameDetailView()
         }
-        .onTapGesture{
-            isFaceUp.toggle()
+    }
+    
+    struct CardView: View {
+        var content: String
+        @State var isFaceUp: Bool = true
+        
+        var body: some View{
+            ZStack {
+                let base = RoundedRectangle(cornerRadius: 12)
+                Group{
+                    base.fill(.white)
+                    base.strokeBorder(lineWidth: 2)
+                    Text(content).font(.largeTitle)
+                }
+                .opacity(isFaceUp ? 1 : 0)
+                base.fill().opacity(isFaceUp ? 0 : 1)
+            }
+            .onTapGesture{
+                isFaceUp.toggle()
+            }
         }
     }
 }
